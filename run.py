127f95accf9f35ed7349a05a4790f1bd9f488922
htanmo <145841395+htanmo@users.noreply.github.com>
2024-10-08 23:33:39 +0530

    added type annotations

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+from flask import Flask
 
-flask_app = create_app()
+flask_app: Flask = create_app()
 
 if __name__ == "__main__":
     flask_app.run("0.0.0.0", debug=True)
