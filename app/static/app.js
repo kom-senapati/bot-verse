9d375e25e5b14cc366146d0f4252a75b12ca689f
RishabxBhardwaj <rishabcgc@gmail.com>
2024-10-13 13:12:00 +0530

    add top back to top  button

--- app/static/app.js
+++ app/static/app.js
@@ -145,3 +145,22 @@ function toggleAnonymousChatbotButton() {
 }
 
 toggleAnonymousChatbotButton();
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var backToTopBtn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
