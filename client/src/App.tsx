311b095446e74a67c140aa2f4a30d17f003cb89a
Priyanshu Verma <priyanshuverma@outlook.in>
2024-10-23 10:51:41 +0000

    feat: A copilot assistant

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,32 +14,37 @@ import NotFound from "./pages/404";
 import AuthProvider from "./contexts/auth-context";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Modals from "./contexts/modals";
+import "@copilotkit/react-ui/styles.css";
+import { CopilotKit } from "@copilotkit/react-core";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Modals />
-        <Toaster />
-        <Routes>
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/anonymous" element={<AnonymousPage />} />
+      <CopilotKit publicApiKey={import.meta.env.VITE_COPILOT_PUBLIC_KEY}>
+        <AuthProvider>
+          <Modals />
+          <Toaster />
+          <Routes>
+            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/anonymous" element={<AnonymousPage />} />
 
-          {/* Protected routes wrapped in ProtectedRoute */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/hub" element={<HubPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/profile/:username" element={<ProfilePage />} />
-            <Route path="/chatbot/:id" element={<ChatbotPage />} />
-            <Route path="/imagine" element={<ImaginePage />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
+            {/* Protected routes wrapped in ProtectedRoute */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/chatbot/:id" element={<ChatbotPage />} />
+              <Route path="/imagine" element={<ImaginePage />} />
+
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/hub" element={<HubPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/profile/:username" element={<ProfilePage />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </CopilotKit>
     </QueryClientProvider>
   );
 }
