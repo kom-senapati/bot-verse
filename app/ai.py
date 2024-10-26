675b7fbf5e2e21fb6348099aced34c2f43c33e4e
Priyanshu Verma <priyanshuverma@outlook.in>
2024-10-26 12:04:28 +0000

    feat: User specific API key adding option

--- app/ai.py
+++ app/ai.py
@@ -1,14 +1,12 @@
-import os
 from groq import Groq
 from dotenv import load_dotenv
 from typing import List, Dict
 
 load_dotenv()
 
-client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
-
-def chat_with_chatbot(messages: List[Dict[str, str]]) -> str:
+def chat_with_chatbot(messages: List[Dict[str, str]], apiKey: str) -> str:
+    client = Groq(api_key=apiKey)
     chat_completion = client.chat.completions.create(
         messages=messages,
         model="llama3-8b-8192",
