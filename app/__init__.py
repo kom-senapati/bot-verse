004adaa7d1e5cd80fa839aa0fb25fee18d7a6c2c
Kom Senapati <komnoob123@gmail.com>
2024-11-24 07:10:51 +0000

    chore: remove flask frontend

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from jinjaMarkdown.markdownExtension import markdownExtension
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
@@ -24,7 +23,6 @@ def create_app() -> Flask:
         "SECRET_KEY", "default_jwt_secret_key"
     )
     app.url_map.strict_slashes = False
-    app.jinja_env.add_extension(markdownExtension)
     # temp. condition
     # TODO: keep only jwt
     jwt = JWTManager(app)
@@ -41,10 +39,8 @@ def create_app() -> Flask:
     def load_user(uid: int) -> User:
         return User.query.get(uid)
 
-    from .routes import register_routes
     from .api_routes import register_api_routes
-
-    register_routes(app, db, bcrypt)
+    
     register_api_routes(app, db, bcrypt)
 
     return app
--- app/static/js/logout.js
+++
@@ -1,2 +0,0 @@
-window.history.pushState(null, "", "/"); // Change the URL without reloading the page
-window.location.reload(); // Reload the page
